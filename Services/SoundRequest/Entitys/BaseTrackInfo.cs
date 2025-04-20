15e4041625f6eab7bc3184638081b791ecb1c4a5
RXDCODX <maxxxes73rus@gmail.com>
2025-04-20 17:00:15 +0400

    ta

---
+++ Services/SoundRequest/Entitys/BaseTrackInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace MARS.Server.Services.SoundRequest.Entitys;
+
+public class BaseTrackInfo
+{
+    [Key]
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    public required string TrackName { get; set; }
+    public string[]? Authors { get; set; }
+    public string[]? FeatAuthors { get; set; }
+    public TimeSpan Duration { get; set; }
+    public string[]? Genre { get; set; }
+}
