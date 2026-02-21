dbc24fdc9eed66339ca5074b64c57bd5226953a9
RXDCODX <maxxxes73rus@gmail.com>
2026-02-21 11:59:03 +0400

    refactor: Упорядочить код миграции для добавления поля VideoUrl в таблицы TekkenMoves и TekkenMovesPending

--- Migrations/20260221070916_AddMoveVideoUrl.cs
+++ Migrations/20260221070916_AddMoveVideoUrl.cs
@@ -2,37 +2,36 @@
 
 #nullable disable
 
-namespace MARS.Server.Migrations
+namespace MARS.Server.Migrations;
+
+/// <inheritdoc />
+public partial class AddMoveVideoUrl : Migration
 {
     /// <inheritdoc />
-    public partial class AddMoveVideoUrl : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "VideoUrl",
-                table: "TekkenMovesPending",
-                type: "text",
-                nullable: true);
+        migrationBuilder.AddColumn<string>(
+            name: "VideoUrl",
+            table: "TekkenMovesPending",
+            type: "text",
+            nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "VideoUrl",
-                table: "TekkenMoves",
-                type: "text",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "VideoUrl",
+            table: "TekkenMoves",
+            type: "text",
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "VideoUrl",
-                table: "TekkenMovesPending");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "VideoUrl",
+            table: "TekkenMovesPending");
 
-            migrationBuilder.DropColumn(
-                name: "VideoUrl",
-                table: "TekkenMoves");
-        }
+        migrationBuilder.DropColumn(
+            name: "VideoUrl",
+            table: "TekkenMoves");
     }
 }
