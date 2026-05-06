95373f01b1598177c236eecc993d8cf7bfcafd76
RXDCODX <maxxxes73rus@gmail.com>
2026-05-07 00:29:54 +0400

    добавил рикролинг на adhd

--- Services/Twitch/Rewards/2002_AdhdSuperpower/AdhdSuperpower_TwitchReward.cs
+++ Services/Twitch/Rewards/2002_AdhdSuperpower/AdhdSuperpower_TwitchReward.cs
@@ -10,7 +10,8 @@ public class AdhdSuperpower_TwitchReward(
     ILogger<AdhdSuperpower_TwitchReward> logger,
     IHostEnvironment environment,
     IHubContext<TelegramusHub, ITelegramusHub> hubContext,
-    EventSubWebsocketClient wsClient
+    EventSubWebsocketClient wsClient,
+    RickRollerService rickRollerService
 ) : TemporaryReward(channelRewardsService, logger, environment)
 {
     public override string AlertDisplayName { get; set; } = "ADHD Superpower";
@@ -19,25 +20,18 @@ public class AdhdSuperpower_TwitchReward(
     public override int Cost { get; init; } = 2002;
     public override Func<bool> IsRewardEnabled { get; set; } = () => true;
 
-    public bool IsServiceActive { get; set; } = true;
-
     private const int AdhdDurationSeconds = 60;
 
-    public override async Task StartAsync(CancellationToken cancellationToken)
+    public override Task StartAsync(CancellationToken cancellationToken)
     {
-        await base.StartAsync(cancellationToken);
-
-        if (IsServiceActive)
-        {
-            wsClient.ChannelPointsCustomRewardRedemptionAdd +=
-                OnChannelPointsCustomRewardRedemption;
-        }
+        wsClient.ChannelPointsCustomRewardRedemptionAdd += OnChannelPointsCustomRewardRedemption;
+        return base.StartAsync(cancellationToken);
     }
 
-    public override async Task StopAsync(CancellationToken cancellationToken)
+    public override Task StopAsync(CancellationToken cancellationToken)
     {
         wsClient.ChannelPointsCustomRewardRedemptionAdd -= OnChannelPointsCustomRewardRedemption;
-        await base.StopAsync(cancellationToken);
+        return base.StopAsync(cancellationToken);
     }
 
     private async Task OnChannelPointsCustomRewardRedemption(
@@ -45,11 +39,6 @@ public class AdhdSuperpower_TwitchReward(
         ChannelPointsCustomRewardRedemptionArgs args
     )
     {
-        if (!IsServiceActive)
-        {
-            return;
-        }
-
         var twEvent = args.Payload.Event;
 
         if (
@@ -62,18 +51,24 @@ public class AdhdSuperpower_TwitchReward(
         {
             try
             {
-                logger.LogInformation(
-                    "ADHD награда активирована пользователем {UserName} за {Cost} поинтов",
-                    twEvent.UserName,
-                    twEvent.Reward.Cost
-                );
+                await rickRollerService.TryRickRollAsync(
+                    TwitchUser.FromChannelPointsCustomRewardRedemptionArgs(args)!,
+                    async () =>
+                    {
+                        logger.LogInformation(
+                            "ADHD награда активирована пользователем {UserName} за {Cost} поинтов",
+                            twEvent.UserName,
+                            twEvent.Reward.Cost
+                        );
 
-                await hubContext.Clients.All.Adhd(AdhdDurationSeconds);
+                        await hubContext.Clients.All.Adhd(AdhdDurationSeconds);
 
-                logger.LogInformation(
-                    "ADHD эффект активирован на {Duration} секунд для пользователя {UserName}",
-                    AdhdDurationSeconds,
-                    twEvent.UserName
+                        logger.LogInformation(
+                            "ADHD эффект активирован на {Duration} секунд для пользователя {UserName}",
+                            AdhdDurationSeconds,
+                            twEvent.UserName
+                        );
+                    }
                 );
             }
             catch (Exception ex)
