1db2c92762f82c5af215c48191f8312c92755e06
RXDCODX <maxxxes73rus@gmail.com>
2026-05-06 23:53:08 +0400

    feat: добавил сервис рикролла

---
+++ Services/Twitch/Rewards/RickRollerService.cs
@@ -0,0 +1,34 @@
+﻿using MARS.Server.Services.Twitch.Entitys;
+
+namespace MARS.Server.Services.Twitch.Rewards;
+
+public class RickRollerService(
+    IHubContext<TelegramusHub, ITelegramusHub> hubContext,
+    IConfiguration configuration,
+    ITwitchClient client,
+    TwitchUserEnsureService userEnsureService
+)
+{
+    private static readonly Random Rnd = new();
+    public double RickRollChance { get; private set; } =
+        double.Parse(configuration["AppSettings:RickRoll:Chance"] ?? "0.05");
+
+    public async Task<bool> TryRickRollAsync(TwitchUser user, Func<Task> whenNotRickRolled)
+    {
+        var roll = Rnd.NextDouble();
+        if (roll < RickRollChance)
+        {
+            user = await userEnsureService.EnsureUserExistsAsync(user);
+            await hubContext.Clients.All.RickRoll(user);
+            await client.SendMessageToMainTwitchAsync(
+                $"@{user.UserLogin}, прости, но награда тебя рикрольнула! Ничего личного, просто рандом!"
+            );
+            return true;
+        }
+        else
+        {
+            await whenNotRickRolled();
+            return false;
+        }
+    }
+}
