a8de690f5f86301783c976d1f272b39fe7491971
RXDCODX <maxxxes73rus@gmail.com>
2026-06-24 01:29:40 +0400

    refactor(migration): убрал лишние комментарии и исправил форматирование миграции

--- Migrations/20260622214311_AddAliasNicknameToTwitchUser.cs
+++ Migrations/20260622214311_AddAliasNicknameToTwitchUser.cs
@@ -2,28 +2,27 @@
 
 #nullable disable
 
-namespace MARS.Server.Migrations
+namespace MARS.Server.Migrations;
+
+/// <inheritdoc />
+public partial class AddAliasNicknameToTwitchUser : Migration
 {
     /// <inheritdoc />
-    public partial class AddAliasNicknameToTwitchUser : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "AliasNickname",
-                table: "TwitchUsers",
-                type: "character varying(100)",
-                maxLength: 100,
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "AliasNickname",
+            table: "TwitchUsers",
+            type: "character varying(100)",
+            maxLength: 100,
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AliasNickname",
-                table: "TwitchUsers");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AliasNickname",
+            table: "TwitchUsers");
     }
 }
