50082e69563f00f9a4ce192d1d220b963a23ace9
RXDCODX <maxxxes73rus@gmail.com>
2026-08-20 15:31:15 +0400

    refactor(migration): улучшение структуры кода в миграции AddShowTimerAndDvdLogosCount

--- Migrations/20260819123119_AddShowTimerAndDvdLogosCount.cs
+++ Migrations/20260819123119_AddShowTimerAndDvdLogosCount.cs
@@ -2,39 +2,38 @@
 
 #nullable disable
 
-namespace MARS.Server.Migrations
+namespace MARS.Server.Migrations;
+
+/// <inheritdoc />
+public partial class AddShowTimerAndDvdLogosCount : Migration
 {
     /// <inheritdoc />
-    public partial class AddShowTimerAndDvdLogosCount : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "DvdLogosCount",
-                table: "AdhdLayoutConfig",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
+        migrationBuilder.AddColumn<int>(
+            name: "DvdLogosCount",
+            table: "AdhdLayoutConfig",
+            type: "integer",
+            nullable: false,
+            defaultValue: 0);
 
-            migrationBuilder.AddColumn<bool>(
-                name: "ShowTimer",
-                table: "AdhdLayoutConfig",
-                type: "boolean",
-                nullable: false,
-                defaultValue: false);
-        }
+        migrationBuilder.AddColumn<bool>(
+            name: "ShowTimer",
+            table: "AdhdLayoutConfig",
+            type: "boolean",
+            nullable: false,
+            defaultValue: false);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "DvdLogosCount",
-                table: "AdhdLayoutConfig");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "DvdLogosCount",
+            table: "AdhdLayoutConfig");
 
-            migrationBuilder.DropColumn(
-                name: "ShowTimer",
-                table: "AdhdLayoutConfig");
-        }
+        migrationBuilder.DropColumn(
+            name: "ShowTimer",
+            table: "AdhdLayoutConfig");
     }
 }
