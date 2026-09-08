6fafe8b02c7e93142d77c513329ca7f404503e80
RXDCODX <maxxxes73rus@gmail.com>
2026-09-09 01:06:26 +0400

    refactor: переработать !ttsvoice для случайной смены голоса только в Twitch

--- Services/CommandExecutor/Commands/!ttsvoice_command.cs
+++ Services/CommandExecutor/Commands/!ttsvoice_command.cs
@@ -1,5 +1,6 @@
 using MARS.Server.Services.CommandExecutor.Entitys;
 using MARS.Server.Services.CommandExecutor.Entitys.Commands;
+using MARS.Server.Services.Twitch.Entitys;
 using MARS.Server.Services.Twitch.Synthesizer;
 
 namespace MARS.Server.Services.CommandExecutor.Commands;
@@ -10,19 +11,44 @@ public class TtsVoiceCommand(ITtsHubBroadcaster broadcaster) : BaseCommand
     public override string Description => "Случайно сменить голос TTS";
     public override bool IsAdminCommand => false;
 
+    public override Platform[] AvailablePlatforms => [Platform.Twitch];
+
     public override async Task<string> ExecuteAsync(
         Dictionary<string, object> parameters,
         Platform platform = Platform.None,
         CancellationToken cancellationToken = default
     )
     {
-        if (!parameters.TryGetValue("userId", out var userIdObj) || userIdObj is not string userId)
+        var result = "Не удалось определить ваш идентификатор.";
+
+        var userId = ResolveUserId(parameters);
+
+        if (!string.IsNullOrWhiteSpace(userId))
         {
-            return "Не удалось определить ваш идентификатор.";
+            await broadcaster.BroadcastReassignVoiceAsync(userId, cancellationToken);
+            result = "Голос случайно изменен";
         }
 
-        await broadcaster.BroadcastReassignVoiceAsync(userId, cancellationToken);
+        return result;
+    }
 
-        return "Голос случайно изменен";
+    private static string? ResolveUserId(Dictionary<string, object> parameters)
+    {
+        string? result = null;
+
+        if (parameters.TryGetValue("user", out var userObj) && userObj is TwitchUser user)
+        {
+            result = user.TwitchId;
+        }
+        else if (
+            parameters.TryGetValue("userId", out var userIdObj)
+            && userIdObj is string userId
+            && !string.IsNullOrWhiteSpace(userId)
+        )
+        {
+            result = userId;
+        }
+
+        return result;
     }
 }
